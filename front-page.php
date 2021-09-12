536c2d34b95206aaa2f6f22c91b80b2761a4f1b1
Zafor Iqbal <zaforiqbal13@gmail.com>
2021-09-13 05:49:16 +0600

    update files

--- front-page.php
+++ front-page.php
@@ -7,3 +7,149 @@
 
 get_header();
 ?>
+
+
+<section class="our-story-one bg-padding-top">
+<div class="container">
+    <div class="row ">
+        <div class="our-story-content center-content text-center">
+            <h1 class="font-size-56"> Welcome to coreplus</h1>
+            <p>We are all about making life easy for every Australian health provider.</p>
+            <div class="center-content">
+                <div class="our-story-hero-image">
+                    <img src="images/our-story.svg"  alt="">
+                </div>
+
+            </div>
+            
+        </div>
+
+    </div>
+</div>
+</section>
+
+<section class="our-story-two bg-spacing-top">
+    <div class="container text-center">
+        <div class="section-spacing">
+            <h1 class="font-size-56">Who We Are</h1>
+            <p class="bg-spacing-top-30 ">Since its inception in 2009, coreplus quickly became a standout in the allied health space with a drive to connect and align with the evolving Australian digital health landscape and the needs of the service providers within the sphere. At present, thousands of medical practitioners across Australia use coreplus everyday to transfer their tedious paper-based workflows to the digital health sphere and dedicate themselves to growing their business and focusing their time towards what really matters, providing quality healthcare to their patients.</p>
+
+          <div class="download-brandbook">
+            <a href="#"> Download Brandbook &nbsp;&nbsp; <i class="fas fa-cloud-download-alt"></i></a>
+          </div>
+               
+           
+        </div>
+
+    </div>
+</section>
+
+<section>
+    <div class="container bg-spacing-top">
+        <hr class="border-color" >
+    </div>
+</section>
+
+<section class="who-we-are">
+    <div class="container ">
+        <div class="row">
+            <div class="col-lg-5 col-md-7 col-sm-12 center-content">
+                <div class="who-we-are-image">
+                    <img src="images/practitioners.svg" alt="">
+                </div>
+                
+            </div>
+            <div class="col-lg-5 col-md-7 col-sm-12 center-content">
+                <div class="text-center">
+                    <h1>Who We Serve</h1>
+                </div>
+                
+                <p>Coreplus caters to all the modern health professionals who are seeking digital tools that enable them to manage their practice efficiently and effectively, provide quality patient care and much more.</p>
+                <p><b><i style="color: black;">
+                    “It doesn’t matter what type of health provider you are, coreplus can scale and grow to suit your needs now and into the future, it’s never been easier.”</i></b>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <hr class="border-color" >
+    </div>
+</section>
+
+<section class="mission-vision">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 ">
+                <div class="mv-div our-mission">
+                    <img src="/images/mission.svg" class="similar-height" alt="">
+                    <h5>Our Mission</h5>
+                    <p>To enable Allied Health Practices to utilize their preferred tools to improve clinical outcomes for patients.</p>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="mv-div">
+                    <img class="similar-height" src="/images/vision.svg" alt="">
+                    <h5>Our Vision</h5>
+                    <p>To be the Australian leader in online health patient & practice management software as a service.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section>
+    <div class="container">
+        <hr class="border-color" >
+    </div>
+</section>
+
+<section>
+    <div class="container"></div>
+    <div class="row">
+        <div  class="text-center what-we-believe">
+            <h1>What We Believe</h1>
+            <div style="max-width: 700px; margin:auto;">
+                <p style="margin-top: 50px;">At coreplus, our core purpose is to connect, help and grow Australia's health providers using our core online patient & practice management system. The plus represents innovation supporting health billing & claiming, online add-ons, patient referrals & eHealth readiness. This is all so you, can have a "Practice Happy” experience.</p>
+
+            </div>
+            <div style="max-width: 700px; margin:auto;">
+                <p><b><i style="color: black;">
+                    By implementing and using coreplus and its partner network, you are freeing up time and money lost on admin, bespoke systems and managing inefficiency and ineffectiveness, creating the right dynamics so that, now and into the future, you “Practice Happy”.
+                 </i> </b> </p>
+            </div>
+            
+        </div>
+
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <hr class="border-color" >
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="our-story-video text-center">
+                <img src="images/our-story/video.svg" alt="">
+            </div>
+            <div class="our-brand-values text-center">
+                <a href="#">Our Brand Values &#8594;</a>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+<?php 
+
+get_footer();
+?>
--- our-values.php
+++ our-values.php
@@ -6,3 +6,91 @@
 
 get_header();
 ?>
+
+<section class="our-story-one">
+<div class="container">
+    <div class="row our-values-hero">
+        <div class="col-md-6 our-values-content">
+            <div>
+                <h1>We Are coreplus</h1>
+                <p>Our success to date is based on having great people who are passionate, focused, hardworking, innovative and fun. We band together to support each other, our customers and most importantly help each other grow. </p>
+            </div>
+                <div class="download-brandbook">
+                    <a href="#" > Download Brandbook &nbsp;&nbsp; <i class="fas fa-cloud-download-alt"></i></a>
+                </div>
+        </div>
+        <div class=" col-md-6">
+            <div class="our-values-hero-image">
+                <img src="images/our-values.svg" alt="">
+            </div>
+        </div>
+
+    </div>
+</div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row our-values">
+            <div class="col-md-6">
+                <div class="our-values-grid equal-grids">
+                    <div class="values-icon ">
+                        <img src="images/my-practice.svg" alt="">
+                    </div>
+    
+                    <div>
+                        <h5>My Practice</h5>
+                        <p>Our platform represents a solid foundation for your healthcare business to connect safely, seamlessly and privately with referral programs and remain agile and adaptive to an evolving healthcare system and consumers needsy.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="our-values-grid equal-grids">
+
+                    <div class="values-icon ">
+                        <img src="images/my-clients.svg" alt="">
+                    </div>
+    
+                    <div>
+                        <h5>My Clients</h5>
+                        <p>Client’s know when your practice is running smoothly & efficiently. Take advantage of cloud-based technology to empower your daily workflow and improve client outcomes with smarter online software.</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class=" col-md-6 ">
+                <div class="our-values-grid equal-grids">
+                    <div class="values-icon ">
+                        <img src="images/my-team.svg" alt="">
+                    </div>
+                    <div>
+                        <h5>My Team</h5>
+                        <p>A 24/7 Support Center and backed up by our very own customer chatbot Corey, our users are always guided  to a way forward and provided a solution to any issue that may arise when operating their practices via coreplusy. </p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-md-6 ">
+                <div class="our-values-grid equal-grids">
+                    <div class="values-icon ">
+                        <img src="images/myself.svg" alt="">
+                    </div>
+                    <div>
+                        <h5>My Self</h5>
+                        <p>We are a passionate team that believes every health professional in Australia deserves to practice happy.</p>
+                        <div><br></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="our-brand-values text-center">
+            <a href="#">Brand Assets Guide &#8594;</a>
+        </div>
+        
+    </div>
+</section>
+
+<?php get_footer(); ?>
